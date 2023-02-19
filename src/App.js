0ef3dd9fcee44bbc6f6782c200955601bcf2fa30
Rehmet Yeshanew <ryfgmz87@gmail.com>
2023-02-19 16:05:48 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { UFix64, UInt64 } from '@onflow/types';
+
+function App() {
+  return (
+    <div className="App">
+      Hello flow
+    </div>
+  );
+}
+
+export default App;
